e51fdb3643159d219d00428a797806dc00abc0ae
Christopher Pritchard <Christopher Pritchard>
2016-12-31 09:07:01 +1300

    Initial work setting up a new dotnet core website

---
+++ GrislyGrotto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GrislyGrotto
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ GrislyGrotto/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace GrislyGrotto
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+
+            if(env.IsDevelopment())
+                builder.AddUserSecrets();
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationScheme = "Cookie",
+                LoginPath = new PathString("/Login"),
+                AccessDeniedPath = new PathString("/Login"),
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true
+            });
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
