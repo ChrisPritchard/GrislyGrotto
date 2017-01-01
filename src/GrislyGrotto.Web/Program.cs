0b9ed9b13fa217b453d7702e36766cf544e39f5c
Christopher Pritchard <Christopher Pritchard>
2017-01-02 09:37:27 +1300

    Added IIS integration files and settings

--- src/GrislyGrotto.Web/Program.cs
+++ src/GrislyGrotto.Web/Program.cs
@@ -14,6 +14,7 @@ namespace GrislyGrotto
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
