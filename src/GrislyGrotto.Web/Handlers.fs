27078e8e25ca2c2ab0221037622522cd2123dce8
Christopher <chrispritchard.nz@gmail.com>
2018-06-02 09:48:39 +1200

    Cleaned up how the handler was written Now more giraffe-like, matching sample application

--- src/GrislyGrotto.Web/Handlers.fs
+++ src/GrislyGrotto.Web/Handlers.fs
@@ -1,11 +1,17 @@
 module Handlers
-open Data
-open Giraffe.ResponseWriters
+open Giraffe
+open Microsoft.AspNetCore.Http
 
-let latest (data: GrislyData) = 
-    query {
-        for post in data.FullPosts () do
-            sortByDescending post.Date
-            take 5
-            select post
-    } |> Seq.toList |> List.map Views.listPost |> Views.layout [] |> htmlView
+let latest = 
+    fun (next : HttpFunc) (ctx : HttpContext) -> 
+        task {
+            let data = ctx.GetService<Data.GrislyData> ()
+            let posts = query {
+                for post in data.FullPosts () do
+                    sortByDescending post.Date
+                    take 5
+                    select post
+            }
+            let html = posts |> Seq.toList |> List.map Views.listPost |> Views.layout []
+            return! htmlView html next ctx
+        }
