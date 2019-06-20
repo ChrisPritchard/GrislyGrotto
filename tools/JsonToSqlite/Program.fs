2e3d3ae09b5fb9fe6f6d344c797213d9de21fc60
Christopher <chrispritchard.nz@gmail.com>
2019-06-20 21:14:55 +1200

    implemented post copying

--- tools/JsonToSqlite/Program.fs
+++ tools/JsonToSqlite/Program.fs
@@ -9,10 +9,10 @@ let main argv =
     match argv with
     | [|jsonDirectory;databasePath|] ->
         
-        let json = Directory.GetFiles (jsonDirectory, "*.json")
+        let json = Directory.GetFiles (jsonDirectory, "*.json") |> Array.rev |> Array.take 10
         printfn "found %i files" json.Length
 
-        File.Copy ("./blank_grislygrotto.db", databasePath)
+        File.Copy ("./blank_grislygrotto.db", databasePath, true)
         printfn "copied db template"
 
         let options = DbContextOptionsBuilder().UseSqlite("Data Source=" + databasePath).Options
@@ -24,7 +24,10 @@ let main argv =
         else
             for file in json do
                 let post = File.ReadAllText file |> JsonConvert.DeserializeObject<Post>
-                dbContext.Posts.Add()
+                printfn "processing '%s'" post.Title
+                dbContext.Posts.Add post |> ignore
+                dbContext.SaveChanges () |> ignore
+            ()
 
     | _ -> 
         printfn "usage: [json extract dir] [path to save filled db]"
