240c54efde0b44b03c687541fbaeb821ceddb101
Christopher <chrispritchard.nz@gmail.com>
2019-06-21 13:10:45 +1200

    wrote sqlite backup tool sqlite to json

---
+++ tools/SqliteToJson/Program.fs
@@ -0,0 +1,37 @@
+﻿open System.IO
+open Newtonsoft.Json
+open GrislyGrotto.Data
+open Microsoft.EntityFrameworkCore
+
+[<EntryPoint>]
+let main argv =
+    
+    match argv with
+    | [|databasePath;jsonDirectory|] ->
+
+        let options = DbContextOptionsBuilder().UseSqlite("Data Source=" + databasePath).Options
+        use dbContext = new GrislyData(options)
+
+        if not (dbContext.Database.CanConnect ()) then
+            printfn "could not connect to db :("
+            ()
+        else
+            let posts = 
+                dbContext.Posts.Include(fun o -> o.Author)
+                    .Include(fun o -> o.Comments)
+                    .ToArrayAsync().GetAwaiter().GetResult();
+            printfn "found %i posts" posts.Length
+
+            for i in [0..posts.Length - 1] do
+                let post = posts.[i]
+                printfn "processing %i of %i: '%s'" (i + 1) posts.Length post.Title
+                let json = JsonConvert.SerializeObject(post)
+                let fileName = sprintf "%s-%s.json" (post.Date.ToString("yyyy-MM-dd-hh-mm-ss-tt")) post.Key
+                let path = Path.Combine(jsonDirectory, fileName);
+                File.WriteAllText(path, json)
+            ()
+
+    | _ -> 
+        printfn "usage: [path to source db] [json extract dir]"
+
+    0
