10886d8005b12713a3ab87679c6b451b9ba46f35
Christopher <chrispritchard.nz@gmail.com>
2020-03-14 13:16:29 +1300

    added the wandering triangles back in the background!

--- src/static/site.js
+++ src/static/site.js
@@ -1,3 +1,11 @@
+let anim = new GrislyGrotto.BackgroundAnimation()
+anim.entityCount = 50
+anim.initialise(
+        document.getElementById('background'), 
+        'black', 
+        '#9acc14', 
+        'grey')
+
 confirmElems = document.getElementsByClassName("confirm-click");
 for (var i in confirmElems) {
     confirmElems[i].addEventListener("click", function () {
