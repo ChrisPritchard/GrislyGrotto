f323d0fdb6bf6548a13099836170ddadf4e02c25
Christopher <chrispritchard.nz@gmail.com>
2020-05-02 11:40:03 +1200

    added a prompt for leaving the editor page when its dirty

--- src/static/site.js
+++ src/static/site.js
@@ -6,9 +6,33 @@ anim.initialise(
         '#9acc14', 
         'grey')
 
+// Confirm elements, e.g. deleting a comment
+
 confirmElems = document.getElementsByClassName("confirm-click");
 for (var i = 0; i < confirmElems.length; i++) {
     confirmElems[i].addEventListener("click", function () {
         return confirm("Are you sure? This action is irreversible!");
     });
+}
+
+// Dirty flag and confirm leave on editor page, to stop 'accidents'
+
+let title = document.getElementById('title');
+let content = document.getElementById('content');
+if (title && content) {
+    let dirty = false
+    title.onchange = function () { dirty = true; }
+    content.onchange = function () { dirty = true; }
+    document.getElementById('submit').onclick = function () {
+        dirty = false;
+    }
+    window.onbeforeunload = function () {
+        if (dirty) {
+            // note: most browsers ignore this and just detect that I return anything at all
+            // if I do (e.g. the below 'confirm' object), then they present their own version of the below
+            // I could return 1 here, or true, or even false, and it would trigger a prompt
+            return confirm('are you sure you want to leave?')
+        }
+        // by returning nothing here, there is no prompt. note that return false here DOES trigger a prompt
+    }
 }
