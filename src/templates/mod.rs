6daf5eeeab4f6228128db8b2ea180871f40b7c24
Christopher <chris@grislygrotto.nz>
2023-04-22 13:54:10 +1200

    master page through extend

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,5 +1,6 @@
 use tera::Tera;
 
+const MASTER_PAGE: &str = include_str!("master.html");
 const PAGE_LATEST: &str = include_str!("latest.html");
 const PAGE_SINGLE: &str = include_str!("single.html");
 
@@ -7,6 +8,8 @@ const PARSE_ERROR: &str = "template parsing failed";
 
 pub fn template_engine() -> Tera {
     let mut tera = Tera::default();
+
+    tera.add_raw_template("master", MASTER_PAGE).expect(PARSE_ERROR);
 
     tera.add_raw_template("latest", PAGE_LATEST).expect(PARSE_ERROR);
     tera.add_raw_template("single", PAGE_SINGLE).expect(PARSE_ERROR);
