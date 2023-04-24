ace9d870792b2171e8add5702425ed7c31259c92
Christopher <chris@grislygrotto.nz>
2023-04-24 21:05:13 +1200

    started search, with no xss

--- src/handlers/search.rs
+++ src/handlers/search.rs
@@ -1,8 +1,19 @@
 use super::prelude::*;
 
+#[derive(Deserialize)]
+struct SearchInfo {
+    search_term: Option<String>,
+}
+
 #[get("/search")]
-async fn search_page(tmpl: Data<Tera>) -> impl Responder {
-    let context = tera::Context::new();
+async fn search_page(tmpl: Data<Tera>, query: Query<SearchInfo>) -> impl Responder {
+    let mut context = tera::Context::new();
+    if query.search_term.is_some() {
+        context.insert("search_term", &query.search_term);
+    }
+
+    context.insert("zero_results", &true);
+
     let html = tmpl.render("search", &context).expect("template rendering failed");
     HttpResponse::Ok().body(html)
 }
