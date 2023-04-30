1605f8a0ed1ea3abcab70810c6d1702948937c04
Christopher <chris@grislygrotto.nz>
2023-04-30 16:29:11 +1200

    using js to change style

--- src/static/site.js
+++ src/static/site.js
@@ -1,4 +1,6 @@
 
-document.addEventListener("readystatechange", _ => {
-   console.log("ready state changed");
-});
+document.querySelectorAll(".style_selector").forEach(item => 
+   item.addEventListener("click", e => {
+   let style = e.target.getAttribute("data-style");
+   fetch('/style/'+style).then(_ => window.location.reload());
+}));
