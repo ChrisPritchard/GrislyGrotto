4fa9a8f4a965f9cc87204d55bd6af447c2daab63
Christopher <chris@grislygrotto.nz>
2024-05-23 13:58:18 +1200

    implemented a small hack to approximate timezone changes in nz

--- src/local_time.rs
+++ src/local_time.rs
@@ -1,9 +1,11 @@
-use time::OffsetDateTime;
+use time::{macros::offset, Month, OffsetDateTime};
 
 // just used because time crate fails in NZ daylight savings time. should be replaced in the future maybe
 
 pub fn get_now() -> OffsetDateTime {
     let utc_time = OffsetDateTime::now_utc();
-    let local_time = utc_time.to_offset(time::macros::offset!(+13)); // hack hack hack
+    let month = utc_time.month() as u8;
+    let offset = if month < Month::April as u8 || month > Month::September as u8 { offset!(+13) } else { offset!(+12) };
+    let local_time = utc_time.to_offset(offset); // hack hack hack
     local_time
 }
