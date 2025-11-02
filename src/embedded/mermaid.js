d1f072ffff736bc921e60e5b345b4c5662d0ab7a
Chris <chris@grislygrotto.nz>
2025-11-03 10:01:11 +1300

    fixed style and integrated support for page theme

--- src/embedded/mermaid.js
+++ src/embedded/mermaid.js
@@ -1,1 +1,9 @@
-mermaid.initialize({ startOnLoad: true });
+var style = "default";
+if (document.querySelector("#dark-style")) {
+    style = "dark";
+}
+
+mermaid.initialize({
+    theme: style,
+    startOnLoad: true
+});
--- src/embedded/style.js
+++ src/embedded/style.js
@@ -1,6 +1,6 @@
 
-document.querySelectorAll(".style_selector").forEach(item => 
-   item.addEventListener("click", e => {
-   let style = e.target.getAttribute("data-style");
-   fetch('/style/'+style).then(_ => window.location.reload());
-}));
+document.querySelectorAll(".style_selector").forEach(item =>
+    item.addEventListener("click", e => {
+        let style = e.target.getAttribute("data-style");
+        fetch('/style/' + style).then(_ => window.location.reload());
+    }));
